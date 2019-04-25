663a17a0ac48ffdbb3c0ec6cbc695c646593dc08
Yaz96 <migyaz96@hotmail.com>
2019-04-25 07:56:02 -0500

    ejercicio

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://ClementeYanez:gearsofwar96.@clementeyanez-cwiai.mongodb.net/sports?retryWrites=true' 
+exports.PORT = process.env.PORT || 8080;
+
+
+
+
